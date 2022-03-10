296b0fd07974db80c60d0fcd733c35b8ff80404e
jesspesale <jess_pesale@aol.com>
2022-03-09 21:32:25 -0500

    have lists rendering links to the actual list and list items

--- TodoTracker-frontend/src/components/Lists.js
+++ TodoTracker-frontend/src/components/Lists.js
@@ -3,7 +3,7 @@ import {useSelector, useDispatch} from "react-redux"
 import { fetchLists } from '../redux/listActions';
 import List from './List';
 import { FETCH_LISTS } from '../redux/constants';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Lists() {
   const lists = useSelector(state => state.lists);
@@ -22,7 +22,12 @@ console.log(lists)
       return (
         <div>
             {lists.map((list) => {
-              return <List list={list} key={list.id} />;
+              return(
+                <Link key={list.id} to={`/lists/${list.id}`} list={list}>{list.title}</Link>
+
+                // <List list={list} key={list.id} />;
+              )
+
             })}
         </div>
       );
