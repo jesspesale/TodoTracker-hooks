5e86104351e4e924c264dc59e9e149cd17a49a49
jesspesale <jess_pesale@aol.com>
2022-03-11 17:48:47 -0500

    added lists css file and took away underline for List titles as links

--- TodoTracker-frontend/src/components/Lists/Lists.js
+++ TodoTracker-frontend/src/components/Lists/Lists.js
@@ -2,6 +2,7 @@ import {useEffect} from 'react'
 import {useSelector, useDispatch} from "react-redux"
 import { fetchLists } from '../../redux/listActions'
 import { Link } from "react-router-dom"
+import "./Lists.css"
 
 export default function Lists() {
   const lists = useSelector(state => state.lists)
@@ -18,6 +19,7 @@ export default function Lists() {
             {Array.isArray(lists) && lists.map((list) => {
               return (
                 <Link
+                  className='list-link'
                   key={list.id}
                   to={`/lists/${list.id}`}
                 >
