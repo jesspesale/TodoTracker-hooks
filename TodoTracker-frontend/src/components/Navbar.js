b90bc45d7bdc63683874b1055519eefbdeda874c
jesspesale <jess_pesale@aol.com>
2022-03-11 17:45:33 -0500

    moved navbar and links into own divs and fixed css

--- TodoTracker-frontend/src/components/Navbar.js
+++ TodoTracker-frontend/src/components/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import "../App.css"
 import {Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>
-      <Link to="/" className="nav">
-        Home
-      </Link>
-      <Link to="/lists" className="nav">
-        All Lists
-      </Link>
-      <Link to="/lists/new" className="nav">
-        Create a new List
-      </Link>
+    <div className="navbar">
+      <div>
+        <Link to="/" className="nav">
+          Home
+        </Link>
+      </div>
+      <div>
+        <Link to="/lists" className="nav">
+          All Lists
+        </Link>
+      </div>
+      <div>
+        <Link to="/lists/new" className="nav">
+          Create a new List
+        </Link>
+      </div>
     </div>
   );
 }
