540af3756d0f3d2827f2c0f10b3b01850b1a3500
jesspesale <jess_pesale@aol.com>
2022-03-14 19:55:36 -0400

    added edit list item constant

--- TodoTracker-frontend/src/redux/constants.js
+++ TodoTracker-frontend/src/redux/constants.js
@@ -3,3 +3,4 @@ export const ADD_LIST = "ADD_LIST"
 export const ADD_ITEM = "ADD_ITEM"
 export const DELETE_LIST_ITEM = "DELETE_LIST_ITEM"
 export const DELETE_LIST = "DELETE_LIST"
+export const EDIT_LIST_ITEM = "EDIT_LIST_ITEM";
