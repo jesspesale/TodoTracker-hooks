0db5a2f29dc246ececaafcdef6d84a7f7f1e501a
jesspesale <jess_pesale@aol.com>
2022-03-16 20:41:46 -0400

    added info to the home page

--- TodoTracker-frontend/src/components/Home.js
+++ TodoTracker-frontend/src/components/Home.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export default function Home() {
-  return <div className="list-title">Home</div>
+  return (
+    <>
+      <div className="list-title">Welcome to List Trackr</div>
+      <p className='home-paragraph'>
+        Use this app to create lists that help you keep track of your
+        everyday life. From grocery shopping lists, to things you need
+        to get done after work, to even packing lists for your next 
+        vacation, use List Trackr!
+        
+      </p>
+    </>
+  
+  )
 }
